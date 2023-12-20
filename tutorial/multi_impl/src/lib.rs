89645ae6e105d6d1874d165a7b677dcd28211504
sfauvel <sfauvel@gmail.com>
2023-12-20 14:24:35 +0100

    Simplify samples on multi implementation

--- tutorial/multi_impl/src/lib.rs
+++ tutorial/multi_impl/src/lib.rs
@@ -1,13 +1,19 @@
+// https://bennett.dev/dont-use-boxed-trait-objects-for-struct-internals/
+// https://www.possiblerust.com/pattern/3-things-to-try-when-you-can-t-make-a-trait-object
 
+// A function that use a reference to a Box.
 #[allow(dead_code)]
 fn func_box_report(report: &Box<dyn Report>) -> String {
     report.report()
 }
 
+// Function that use a dynamic trait.
 #[allow(dead_code)]
 fn func_dyn_report(report: &dyn Report) -> String {
     report.report()
 }
+
+// Function that use a generic type implementing a trait.
 #[allow(dead_code)]
 fn func_generic_report<R: Report + ?Sized>(report: &R) -> String {
     report.report()
@@ -27,7 +33,7 @@ fn create(x: u32) -> Box<dyn Report> {
     } else if x == 1 {
         Box::new(NewsArticle::new("Bob", "Something"))
     } else {
-      Box::new(AnonymousArticle::new("Something by X"))
+        Box::new(AnonymousArticle::new("Something by X"))
     }
 }
 
@@ -76,65 +82,46 @@ mod tests {
 
     #[test]
     fn my_test_report() {
-        let article: NewsArticle = NewsArticle::new("Seb", "Something");
-        assert_eq!("Something, by Seb", article.report());
-        let box_article: Box<dyn Report> = Box::new(article);
-        assert_eq!("Something, by Seb", func_box_report(&box_article));
-
-
-        let article: AnonymousArticle = AnonymousArticle::new("Something by X");
-        assert_eq!("Something by X", article.report());
-        let box_article: Box<dyn Report> = Box::new(article);
-        assert_eq!("Something by X", func_box_report(&box_article));
-        
         let article: Box<dyn Report> = create(0);
         assert_eq!("Something, by Seb", article.report());
-        assert_eq!("Something, by Seb", func_box_report(&article));
 
-        let article: Box<dyn Report> = create(1);
-        assert_eq!("Something, by Bob", article.report());
-        assert_eq!("Something, by Bob", func_box_report(&article));
+        assert_eq!("Something, by Seb", create(0).report());
+        assert_eq!("Something, by Bob", create(1).report());
+        assert_eq!("Something by X", create(2).report());
+    }
+
+    #[test]
+    fn my_test_report_with_box_dyn_function() {
+        // Need to put it in a local variable.
+        let box_article: Box<dyn Report> = Box::new(NewsArticle::new("Seb", "Something"));
+        assert_eq!("Something, by Seb", func_box_report(&box_article));
+ 
+        let box_article: Box<dyn Report> = Box::new(AnonymousArticle::new("Something by X"));
+        assert_eq!("Something by X", func_box_report(&box_article));
         
-        let article: Box<dyn Report> = create(2);
-        assert_eq!("Something by X", article.report());
-        assert_eq!("Something by X", func_box_report(&article));
+        assert_eq!("Something, by Seb", func_box_report(&create(0)));
+        assert_eq!("Something, by Bob", func_box_report(&create(1)));
+        assert_eq!("Something by X", func_box_report(&create(2)));
     }
     #[test]
     fn my_test_report_with_dyn_report() {
-        let article: NewsArticle = NewsArticle::new("Seb", "Something");
-        assert_eq!("Something, by Seb", func_dyn_report(&article));
+        assert_eq!("Something, by Seb", func_dyn_report(&NewsArticle::new("Seb", "Something")));
+        assert_eq!("Something by X", func_dyn_report(&AnonymousArticle::new("Something by X")));
 
-        let article: AnonymousArticle = AnonymousArticle::new("Something by X");
-        assert_eq!("Something by X", func_dyn_report(&article));
-
-        let article: Box<dyn Report> = create(0);
         // With `as_ref` we transform `Box<R>` to `&dyn R`
-        article.as_ref();
-        assert_eq!("Something, by Seb", func_dyn_report(article.as_ref()));
-        
-        let article: Box<dyn Report> = create(1);
-        assert_eq!("Something, by Bob", func_dyn_report(article.as_ref()));
-        
-        let article: Box<dyn Report> = create(2);
-        assert_eq!("Something by X", func_dyn_report(article.as_ref()));
+        assert_eq!("Something, by Seb", func_dyn_report(create(0).as_ref()));
+        assert_eq!("Something, by Bob", func_dyn_report(create(1).as_ref()));
+        assert_eq!("Something by X", func_dyn_report(create(2).as_ref()));
     }
     #[test]
     fn my_test_report_with_generic() {
-        let article: NewsArticle = NewsArticle::new("Seb", "Something");
-        assert_eq!("Something, by Seb", func_generic_report(&article));
-
-        let article: AnonymousArticle = AnonymousArticle::new("Something by X");
-        assert_eq!("Something by X", func_generic_report(&article));
+        assert_eq!("Something, by Seb", func_generic_report(&NewsArticle::new("Seb", "Something")));
+        assert_eq!("Something by X", func_generic_report(&AnonymousArticle::new("Something by X")));
 
         // As we return a `Box`, we need to store it in a variable before calling `as_ref`. 
         // Otherwise, the value is freed immediately and we could not get a reference.
-        let article: Box<dyn Report> = create(0);
-        assert_eq!("Something, by Seb", func_generic_report(article.as_ref()));
-        
-        let article: Box<dyn Report> = create(1);
-        assert_eq!("Something, by Bob", func_generic_report(article.as_ref()));
-        
-        let article: Box<dyn Report> = create(2);
-        assert_eq!("Something by X", func_generic_report(article.as_ref()));
+        assert_eq!("Something, by Seb", func_generic_report(create(0).as_ref()));
+        assert_eq!("Something, by Bob", func_generic_report(create(1).as_ref()));
+        assert_eq!("Something by X", func_generic_report(create(2).as_ref()));
     }
 }
